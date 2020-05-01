2438a37f76fa50d8138351ac47ef0d62359c6a3a
Adrian Strozzi <adrianstrozzi@gmail.com>
2020-05-01 16:35:52 -0500

    Created two new files under lib to put our resolvers and define our Query types. This was done to achieve a better workflow in our API. Also imported additional dependencies

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 'use strict'
 
-const { graphql, buildSchema } = require('graphql')
+const { buildSchema } = require('graphql')
 const express = require('express')
 const gqlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.port || 3000
 
-
 // Definiendo el esquema
 
-const schema = buildSchema(`
-    type Query {
-      hello: String
-    }
-  `)
-
-// Configurar los resolvers
-
-const resolvers = {
-  hello: () => {
-    return 'Hola Mundo'
-  }
-}
+const schema = buildSchema(
+  readFileSync(
+    join(__dirname, 'lib', 'schema.graphql'),
+    'utf-8'
+  )
+)
 
 app.use('/api', gqlMiddleware({
   schema: schema,
